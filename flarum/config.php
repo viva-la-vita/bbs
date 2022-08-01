d579074f2aea8b4e69d8e04937716534de95aa98
Songchen Tan <songchen@mit.edu>
2022-08-01 16:43:51 -0400

    Add flarum plugin

--- flarum/config.php
+++ flarum/config.php
@@ -5,9 +5,9 @@
     'driver' => 'mysql',
     'host' => 'mysql',
     'port' => 3306,
-    'database' => 'flarum',
-    'username' => 'flarum',
-    'password' => 'nyaxzRuEhKPe',
+    'database' => getenv("MYSQL_DATABASE"),
+    'username' => getenv("MYSQL_USER"),
+    'password' => getenv("MYSQL_PASSWORD"),
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => '',
@@ -15,7 +15,7 @@
     'engine' => NULL,
     'prefix_indexes' => true,
   ),
-  'url' => 'https://bbs.viva-la-vita.org',
+  'url' => getenv("URL"),
   'paths' =>
   array (
     'api' => 'api',
